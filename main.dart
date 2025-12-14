27aa878debb4835f17453f51caa863bfbd01010f
Ahmed Allam <ahmed.allam@izam.com>
2025-12-14 19:37:32 +0200

    Day(5): Encapsulation

--- main.dart
+++ main.dart
@@ -1,5 +1,21 @@
-import 'animal.dart';
+import 'car.dart';
 
 void main() {
-  Animal.instance
+  final car = Car(
+    12,
+    "Car1",
+  );
+
+  car.setSize(200);
+
+  print(car.getSize());
 }
+
+/// ------------- OOP -------------- ///
+/// (1) - Encapsulation:
+///     -- Hide variables , methods
+///     -- set & get
+/// (2) - Inheritance:
+///       
+/// (3) - Abstraction:
+/// (4) - Polymorphism:
